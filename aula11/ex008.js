1fa9cbee75c4d0e50194f109e3a74553f262465c
Reinaldo <ruanpinheirolima2003@gmail.com>
2022-09-12 14:34:50 -0300

    Create ex008.js

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 62
+console.log (`A velocidade é de ${vel}km/h`)
+if(vel > 65){
+    console.log(`Você ultrapassou o limite. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!!`)
