32ac929042f66d73af18b485139c0c38536a8e07
Ruan Pinheiro Lima <ruanpinheirolima2003@gmail.com>
2022-09-27 15:08:36 -0300

    Create ambiente.js

---
+++ aula14/ambiente.js
@@ -0,0 +1,17 @@
+console.log('Vai começar...')
+
+for (var c = 1;
+    c <= 3;
+    c++){
+        console.log(c)
+    }
+
+console.log('Vai terminar...')
+
+// let i = 0 
+// let f = 10
+// let p = 2
+
+// for(let c = i; c <= f; c += p){
+//     console.log(c)
+// }
