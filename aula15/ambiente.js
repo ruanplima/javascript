38ebf9ace97a7cb203c66801213200d1b0880634
Ruan Pinheiro Lima <ruanpinheirolima2003@gmail.com>
2022-09-28 17:10:59 -0300

    Create ambiente.js

---
+++ aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [5, 8, 2, 9, 3]
+//num[4] = 10 Adiciona um elemento na posição digitada entre os '[]'.
+//num.push(4) Adiciona o elemento ao final do Array.
+//num.length  Acusa o tamanho do Array. (quantas posições tem)
+//num.sort() Coloca os elementos em ordem crescente. ex.: num = [2, 3, 4, 5, 8, 9, 10] 
+num.push(1)
+console.log(num)
+console.log(num.sort())
+console.log(`O vetor tem o total de ${num.length} posições`)
